d70df02eac1230054a5a241abc64c92d34dad51b
choijake0207 <jake.ch0712@gmail.com>
2024-09-14 12:09:25 -0700

    add dynamic transform style

--- src/Components/World.jsx
+++ src/Components/World.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function World() {
-  return (
-    <div>
+export default function World({offset}) {
+    const style = {
+        position: "absolute",
+        transform: `translateX(${offset}px)`,
+        background: "purple"
+      }
       
+
+
+  return (
+    <div className="world" style={style}>
+    <p>Hello</p>
     </div>
   )
 }
