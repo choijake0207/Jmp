d276cb47fd2246953a776780cad47548e96241db
choijake0207 <jake.ch0712@gmail.com>
2024-10-06 18:01:54 -0400

    test gravity

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector("canvas")
+canvas.style.border = "1px solid black"
 const game = canvas.getContext('2d')
-
 
 function Sprite () {
     this.position = {
@@ -11,12 +11,25 @@ function Sprite () {
         w: 20,
         h: 20
     }
-
-    
+    this.velocity = {
+        x: 0,
+        y: 1,
+    }
     this.draw = function draw() {
         game.fillRect(this.position.x, this.position.y, this.dimensions.w, this.dimensions.h)
+    }
+    this.update = function update() {
+        this.draw()
+        this.position.y += this.velocity.y
     }
 }
 
 const sprite = new Sprite ()
 sprite.draw()
+
+function animate () {
+    requestAnimationFrame(animate)
+    game.clearRect(0, 0, canvas.width, canvas.height)
+    sprite.update()
+}
+animate()
